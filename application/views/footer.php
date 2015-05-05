b4607f8144b14ee6693e8439de3dd326e004519e
Didit Sepiyanto <sepiyantodidit@gmail.com>
2015-05-05 16:22:09 +0700

    editor by Didit

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<div class="line">
+	<div class="box" style="height:300px; background-color:#37001C; margin-bottom:-10px">
+	</div>
+	<div class="box" style="height:20px; background-color:#201; margin-bottom:-15px">
+	</div>
+</div>
---
+++ application/views/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Openeye.com</title>
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/components.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/responsee.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/owl-carousel/owl.carousel.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/owl-carousel/owl.theme.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/modernizr.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/responsee.js"></script>
+<!--[if lt IE 9]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+      <![endif]-->
+<style>
+body {
+	margin: 0;
+	padding: 0;
+	background-color:#FFF;
+}
+nav {
+	display: block;
+	width: 100%;
+	height: 50px;
+	background-color: #FFF;
+	border-bottom: 1px #666666 solid;
+}
+nav ul {
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+	width: 480px;
+	height: 50px;
+}
+nav ul li {
+	height: 100%;
+	text-transform: uppercase;
+	display: block;
+	float: left;
+	width: 100px;
+	text-align: center;
+	cursor: pointer;
+	line-height: 50px;
+	opacity: .8;
+}
+nav ul li:hover {
+	opacity: 1;
+	border-bottom: 2px #0099FF solid;
+}
+#head {
+	display: block;
+	width: 100%;
+	height: 100vh;
+	border-bottom: 1px #000 solid;
+}
+.news {
+	height:400px;
+	background-size:cover;
+	background-position:center;
+}
+.marg{
+	margin:10px;
+}
+</style>
+</head>
